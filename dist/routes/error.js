8867477c804476ceb59ac652b9c91eae2c6e6b57
BatiuszkaMaroz <david.prajzner01@gmail.com>
2020-11-02 12:04:18 +0100

    DIST FIX

--- dist/routes/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const error_1 = require("../controllers/error");
-const router = express_1.Router();
-router.use(error_1.notFoundHandler);
-exports.default = router;
-//# sourceMappingURL=error.js.map
